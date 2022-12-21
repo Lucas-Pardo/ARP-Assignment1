67c40fd2b1b21523a329c7b54c76f3ac1fd432d7
Lucas-Pardo <lucas.pardo@hotmail.com>
2022-12-21 22:01:40 +0100

    Slight change to shell script

--- master.sh
+++ master.sh
@@ -2,21 +2,22 @@
 
 ./bin/motorx &
 pid_mx=$(ps | pgrep motorx)
-echo $pid_mx
+# echo $pid_mx
 ./bin/motorz &
 pid_mz=$(ps | pgrep motorz)
-echo $pid_mz
+# echo $pid_mz
 ./bin/world &
 pid_world=$(ps | pgrep world)
-echo $pid_world
+# echo $pid_world
 /usr/bin/gnome-terminal -e ./bin/command 
 pid_cmd=$(ps | pgrep command)
-echo $pid_cmd
+# echo $pid_cmd
 /usr/bin/gnome-terminal -x ./bin/inspection $pid_mx $pid_mz $pid_cmd 
 pid_ins=$(ps | pgrep inspection)
-echo $pid_ins
-./bin/watchdog $pid_mx $pid_mz $pid_world $pid_cmd $pid_ins &
-pid_watch=$(ps | pgrep watchdog)
+# echo $pid_ins
+./bin/watchdog $pid_mx $pid_mz $pid_world $pid_cmd $pid_ins
+echo "Main program exited."
+# pid_watch=$(ps | pgrep watchdog)
 # echo $pid_watch
 # echo $(date +%T)
 # wait $pid_cmd
